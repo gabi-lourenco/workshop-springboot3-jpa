87a5caa82a347cf5677ffcaa82894e1ef7128c93
Gabriele Lourenço <gabrielelourenco000@gmail.com>
2023-02-24 09:39:09 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/jakarta/persistence/SharedCacheMode.java
@@ -0,0 +1,33 @@
+package jakarta.persistence;
+
+public enum SharedCacheMode {
+
+	/**
+     * All entities and entity-related state and data are cached.
+     */
+    ALL, 
+
+    /**
+     * Caching is disabled for the persistence unit.
+     */
+    NONE, 
+
+    /**
+     * Caching is enabled for all entities for <code>Cacheable(true)</code>
+     * is specified.  All other entities are not cached.
+     */
+    ENABLE_SELECTIVE, 
+
+    /**
+     * Caching is enabled for all entities except those for which
+     * <code>Catchable(false)</code> is specified.  Entities for which
+     * <code>Catchable(false)</code> is specified are not cached.
+     */
+    DISABLE_SELECTIVE, 
+
+    /**
+     * 
+     * Caching behavior is undefined: provider-specific defaults may apply.
+     */
+    UNSPECIFIED
+}
